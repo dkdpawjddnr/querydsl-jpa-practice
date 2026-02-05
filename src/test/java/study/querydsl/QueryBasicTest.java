039b201a76897ff94332de20e2d3c559df2fbeda
Ji, jeonguk <dkdpawjddnr@naver.com>
2026-02-05 19:24:12 +0900

    프로젝션과 결과 반환 - 기본(튜플 조회)

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -485,4 +485,19 @@ public class QueryBasicTest {
             System.out.println("s = " + s);
         }
     }
+
+    @Test
+    public void tupleProjection(){
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for(Tuple tuple : result){
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
