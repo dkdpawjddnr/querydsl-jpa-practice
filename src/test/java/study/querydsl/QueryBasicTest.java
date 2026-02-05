83caa6437e63c2391f37eded11ceff860edb6d70
Ji, jeonguk <dkdpawjddnr@naver.com>
2026-02-05 19:56:17 +0900

    Querydsl 빈 생성 - 필드 직접 접근

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -526,4 +526,18 @@ public class QueryBasicTest {
                 .from(member)
                 .fetch();
     }
+
+    @Test
+    public void findDtoByField(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for(MemberDto memberDto : result){
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
 }
