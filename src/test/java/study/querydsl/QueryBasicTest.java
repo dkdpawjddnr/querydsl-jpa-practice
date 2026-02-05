2db8f0759645722a0e0f1aea662a3ff09e2304e0
Ji, jeonguk <dkdpawjddnr@naver.com>
2026-02-05 22:06:12 +0900

    Querydsl 빈 생성 - 생성자 사용

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -562,4 +562,18 @@ public class QueryBasicTest {
             System.out.println("userDto = " + userDto);
         }
     }
+
+    @Test
+    public void findDtoByConstructor(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for(MemberDto memberDto : result){
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
 }
